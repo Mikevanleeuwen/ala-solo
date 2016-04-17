cd9f14f5db0d0bc3bf4fa520deaba0b728595ef4
Mikevanleeuwen <mike@comtrix.nl>
2016-04-17 17:09:45 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="First Layer of GuestBook">
+    <meta name="author" content="Mike">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Adresboek | Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="/css/cover.css" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+
+<div class="site-wrapper">
+
+    <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+
+
+            <div class="inner cover">
+                <h1 class="cover-heading">Adresboek gemeente</h1>
+                <p class="lead">Om ons volledige adres boek te zien registreer of log in</p>
+                <p class="lead">
+                    <a href="#" class="btn btn-lg btn-default">Log in</a>
+                    <a href="#" class="btn btn-lg btn-default">Registreer</a>
+                </p>
+            </div>
+
+
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="../../dist/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
